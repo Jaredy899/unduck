59a07f4b51c0fe14829b03f8fa5a469437a35872
Jaredy899 <76603258+Jaredy899@users.noreply.github.com>
2025-03-01 22:37:26 -0500

    Add web manifest and update Vite PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ export default defineConfig({
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
+      manifest: false,
+      includeAssets: ['search.svg', 'clipboard.svg', 'clipboard-check.svg', 'save.svg', 'check.svg'],
+      manifestFilename: 'manifest.webmanifest',
+      injectManifest: {
+        injectionPoint: undefined
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg}']
+      }
     }),
   ],
   build: {
